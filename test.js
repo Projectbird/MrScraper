0a11be2cdd124894b6a3afcb9e6296e40910e60b
Robert Gabriel <robert_gabriel@outlook.com>
2016-02-24 19:18:15 +0000

    Updated package information

--- test.js
+++ test.js
@@ -4,7 +4,11 @@ var chalk = require('chalk');
 
 mrscraper("http://www.porn.com", function (response) {
 
-    console.log(chalk.blue(response));
+
+    for (i = 0; i < response.length; i++) {
+        console.log(chalk.blue(response[i]));
+    }
+
 
 
 });
